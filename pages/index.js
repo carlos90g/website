1ca1c9fa6892351184d6cf467a34ce4b6b9d8515
Carlos Garcia <>
2020-06-17 09:26:22 -0300

    Push to check deploy

--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,11 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Carlos Garcia
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Software · Music · Random
         </p>
 
         <div className="grid">
