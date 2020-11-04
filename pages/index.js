3db12dd9733473d96a358eb3f1b752bead4553b3
Carlos Garcia <carlos90g@gmail.com>
2020-11-04 01:13:14 -0300

    Removed blog section

--- pages/index.js
+++ pages/index.js
@@ -57,20 +57,6 @@ export default function Home({ allPostsData }) {
         </p>
       </section>
       <section className={utilStyles.headingMd}></section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
-      </section>
     </Layout>
   )
 }
